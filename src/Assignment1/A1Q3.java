5fabd0e1a16ff963dd6e72475601496bca148ce1
leendawamneh <leendawamneh@172.21.65.180>
2018-02-13 16:43:08 -0500

    New Assignment work

---
+++ src/Assignment1/A1Q3.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment1;
+
+import apple.laf.JRSUIConstants;
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.RobotSE;
+import becker.robots.Wall;
+
+/**
+ *
+ * @author leendawamneh
+ */
+public class A1Q3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //create a city for the robot
+        City kitchener = new City();
+
+        //create a robot
+      RobotSE leeno = new RobotSE (kitchener, 1, 2, Direction.SOUTH);
+      
+        //create a wall
+        new Wall (kitchener, 3, 2, Direction.WEST);
+        
+    
+    }
+    
+}
