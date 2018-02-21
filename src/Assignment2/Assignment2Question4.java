d5024b5bb337617773c33e0e468b29318ff0f10f
leendawamneh <leendawamneh@172.21.65.180>
2018-02-21 11:17:57 -0500

    Assignment 2 question 4

---
+++ src/Assignment2/Assignment2Question4.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment2;
+
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.RobotSE;
+import becker.robots.Wall;
+
+/**
+ *
+ * @author leendawamneh
+ */
+public class Assignment2Question4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //create a city
+        City kitchener = new City();
+        
+        //create a robot 
+        RobotSE leeno = new RobotSE(kitchener, 1, 2, Direction.EAST);
+        
+        //create walls
+        new Wall(kitchener, 2, 2, Direction.NORTH);
+        new Wall(kitchener, 2, 3, Direction.NORTH);
+        new Wall(kitchener, 2, 2, Direction.WEST);
+        new Wall(kitchener, 3, 2, Direction.WEST);
+        new Wall(kitchener, 3, 3, Direction.SOUTH);
+        new Wall(kitchener, 3, 2, Direction.SOUTH);
+        new Wall(kitchener, 2, 3, Direction.EAST);
+        new Wall(kitchener, 3, 3, Direction.EAST);
+        new Wall(kitchener, 1, 4, Direction.SOUTH);
+        new Wall(kitchener, 1, 4, Direction.NORTH);
+        new Wall(kitchener, 1, 4, Direction.EAST);
+        new Wall(kitchener, 1, 4, Direction.WEST);
+        new Wall(kitchener, 4, 4, Direction.NORTH);
+        new Wall(kitchener, 4, 4, Direction.EAST);
+        new Wall(kitchener, 4, 4, Direction.SOUTH);
+        new Wall(kitchener, 4, 4, Direction.WEST);
+        new Wall(kitchener, 4, 1, Direction.NORTH);
+        new Wall(kitchener, 4, 1, Direction.EAST);
+        new Wall(kitchener, 4, 1, Direction.SOUTH);
+        new Wall(kitchener, 4, 1, Direction.WEST);
+        new Wall(kitchener, 1, 1, Direction.WEST);
+        new Wall(kitchener, 1, 1, Direction.EAST);
+        new Wall(kitchener, 1, 1, Direction.NORTH);
+        new Wall(kitchener, 1, 1, Direction.SOUTH);
+        
+    }
+    
+}
