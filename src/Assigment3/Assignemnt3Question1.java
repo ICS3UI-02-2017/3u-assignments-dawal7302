19ab5f9166788586fdb7c7a9466d69929d00dcb0
leendawamneh <leendawamneh@172.21.65.180>
2018-02-26 11:18:33 -0500

    Assignment 3 started

--- src/Assigment3/Assignemnt3Question1.java
+++ src/Assigment3/Assignemnt3Question1.java
@@ -5,6 +5,12 @@
  */
 package Assigment3;
 
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.RobotSE;
+import becker.robots.Wall;
+
 /**
  *
  * @author leendawamneh
@@ -15,7 +21,37 @@ public class Assignemnt3Question1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        //create a city
+        City kitchener = new City();
+        
+        //create a robot 
+        RobotSE leeno = new RobotSE(kitchener, 0, 2, Direction.WEST);
+        
+        //create walls
+        new Wall(kitchener, 1, 1, Direction.NORTH);
+        new Wall(kitchener, 1, 2, Direction.NORTH);
+        new Wall(kitchener, 1, 1, Direction.WEST);
+        new Wall(kitchener, 2, 1, Direction.WEST);
+        new Wall(kitchener, 2, 1, Direction.SOUTH);
+        new Wall(kitchener, 2, 2, Direction.SOUTH);
+        new Wall(kitchener, 1, 2, Direction.EAST);
+        new Wall(kitchener, 2, 2, Direction.EAST);
+        
+        
+        
+        //use loops to move around it 
+        int numberOfMoves = 2;
+        if (numberOfMoves <3) {
+            leeno.move(2);
+            leeno.turnLeft();
+            leeno.move(3);
+            leeno.turnLeft();
+            leeno.move();           
+        }
+       
+        }
+       
+       }
     
-}
+    
+
