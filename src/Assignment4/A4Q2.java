889b240af4d27e1234d6ee41e59b2939afa530b5
leendawamneh <leendawamneh@172.21.65.180>
2018-03-05 11:19:01 -0500

    A4Q2 Started

---
+++ src/Assignment4/A4Q2.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author leendawamneh
+ */
+public class A4Q2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Please enter the measurement in inches you wish to convert");
+               
+        //use a scanner to read info
+        Scanner input = new Scanner(System.in);
+        
+        //make a variable to store centimenters
+        int 
+    }
+    
+}
