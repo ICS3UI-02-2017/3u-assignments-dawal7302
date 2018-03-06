696527061fba28b2ecd9facbafb0ecdc9f027271
leendawamneh <leendawamneh@172.21.65.180>
2018-03-06 10:36:07 -0500

    A4Q2 finished

--- src/Assignment4/A4Q2.java
+++ src/Assignment4/A4Q2.java
@@ -17,13 +17,18 @@ public class A4Q2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Please enter the measurement in inches you wish to convert");
+        System.out.println("Please enter the measurement in inches you wish to convert:");
                
         //use a scanner to read info
         Scanner input = new Scanner(System.in);
         
         //make a variable to store centimenters
-        int 
+        int measurementInInches = input.nextInt();
+        double num = 2.54;
+        double measurementInCentimeters =  2.54 * measurementInInches;
+        
+        System.out.println(measurementInInches + " inches is the same as " + measurementInCentimeters + " centimeters.");
+        
     }
     
 }
