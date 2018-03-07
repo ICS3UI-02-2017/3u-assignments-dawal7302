4fc2f415ce1df87b66d92f36195caec29c007adb
leendawamneh <leendawamneh@172.21.65.180>
2018-03-07 10:33:46 -0500

    A4Q4 finished

--- src/Assignment4/A4Q4.java
+++ src/Assignment4/A4Q4.java
@@ -20,28 +20,32 @@ public class A4Q4 {
         
         //use a scanner to read information
         Scanner input = new Scanner(System.in);
+        
+        
+        //make integers for the costs
+        System.out.print("How much does the food for prom cost?:");
         int costOne = input.nextInt();
-        System.out.println("How much does the food for prom cost?:" + costOne);
         
+        System.out.print("How much does the DJ cost?:");
         int costTwo = input.nextInt();
-        System.out.println("How much does the DJ cost?:" + costTwo);
         
+        System.out.print("How much does it cost to rent the hall?:");
         int costThree = input.nextInt();
-        System.out.println("How much does it cost to rent the hall?:" + costThree);
         
+        System.out.print("How much does decoration cost?:");
         int costFour = input.nextInt();
-        System.out.println("How much does decoration cost?:" + costFour);
         
+        System.out.print("How much does it cost for staff?:");
         int costFive = input.nextInt();
-        System.out.println("How much does it cost for staff?:" + costFive);
         
+        System.out.print("How much for miscellaneous costs?:");
         int costSix = input.nextInt();
-        System.out.println("How much for miscellaneous costs?:" + costSix);
+        
         
         int totalCost = costOne + costTwo + costThree + costFour + costFive + costSix;
         int numberOfTickets = totalCost / 35;
         
-        System.out.println("The total cost is" + " " + "$" + totalCost + ". You will ned to sell" + numberOfTickets );
+        System.out.println("The total cost is" + " " + "$" + totalCost + ". You will ned to sell" + " " + numberOfTickets + " number of tickets to break even.");
                 
         
         
