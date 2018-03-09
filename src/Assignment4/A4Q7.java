9d193f27b8ed34f2310162be1b905b3f8fad165c
leendawamneh <leendawamneh@172.21.65.180>
2018-03-08 21:08:36 -0500

    A4Q7 finished

--- src/Assignment4/A4Q7.java
+++ src/Assignment4/A4Q7.java
@@ -19,51 +19,70 @@ public class A4Q7 {
     public static void main(String[] args) {
         // create a scanner to store information
         Scanner input = new Scanner(System.in);
-        
+    
        
         System.out.print("Enter sum of dice:");
         int sumOfDiceOne = input.nextInt();
         int squareNumberOne = 1 + sumOfDiceOne;
         System.out.println("You are now on square" + " " + squareNumberOne);
-        
+        if(2 >= sumOfDiceOne && 12 <= sumOfDiceOne) {
+            System.out.print("You Quit!");
+        }
         System.out.print("Enter sum of dice:");
         int sumOfDiceTwo = input.nextInt();
         int squareNumberTwo = 10 + sumOfDiceTwo;
         System.out.println("You are now on square" + " " + squareNumberTwo);
-        
+        if(2 >= sumOfDiceTwo && 12 <= sumOfDiceTwo) {
+            System.out.print("You Quit!");
+        }
         System.out.print("Enter sum of dice:");
         int sumOfDiceThree = input.nextInt();
         int squareNumberThree = 21 + sumOfDiceThree;
         System.out.println("You are now on square" + " " + squareNumberThree);
-        
+        if(2 >= sumOfDiceThree && 12 <= sumOfDiceThree) {
+            System.out.print("You Quit!");
+        }
         System.out.print("Enter sum of dice:");
         int sumOfDiceFour = input.nextInt();
         int squareNumberFour = 57 + sumOfDiceFour;
         System.out.println("You are now on square" + " " + squareNumberFour);
-        
+        if(2 >= sumOfDiceFour && 12 <= sumOfDiceFour) {
+            System.out.print("You Quit!");
+        }
         System.out.print("Enter sum of dice:");
         int sumOfDiceFive = input.nextInt();
         int squareNumberFive = 83 + sumOfDiceFive;
         System.out.println("You are now on square" + " " + squareNumberFive);
-        
+        if(2 >= sumOfDiceFive && 12 <= sumOfDiceFive) {
+            System.out.print("You Quit!");
+        }
         System.out.print("Enter sum of dice:");
         int sumOfDiceSix = input.nextInt();
         int squareNumberSix = 86 + sumOfDiceSix;
         System.out.println("You are now on square" + " " + squareNumberSix);
-        
+        if(2 >= sumOfDiceSix && 12 <= sumOfDiceSix) {
+            System.out.print("You Quit!");
+        }
         System.out.print("Enter sum of dice:");
         int sumOfDiceSeven = input.nextInt();
         int squareNumberSeven = 81 + sumOfDiceSeven;
         System.out.println("You are now on square" + " " + squareNumberSeven);
-        
+        if(2 >= sumOfDiceSeven && 12 <= sumOfDiceSeven) {
+            System.out.print("You Quit!");
+        }
         System.out.print("Enter sum of dice:");
         int sumOfDiceEight = input.nextInt();
         int squareNumberEight = 91 + sumOfDiceEight;
         System.out.println("You are now on square" + " " + squareNumberEight);
-        
+        if(2 >= sumOfDiceEight && 12 <= sumOfDiceEight) {
+            System.out.print("You Quit!");
+        }
+            
         System.out.println("You win!");
         }
-    }
+        }
+        
+        
     
     
 
