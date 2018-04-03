d2984598a638c34ebf65155efc898b189b028c81
leendawamneh <leendawamneh@172.21.65.180>
2018-04-03 11:17:08 -0400

    working on A5Q1

--- src/Assignment5/A5Q1.java
+++ src/Assignment5/A5Q1.java
@@ -23,24 +23,36 @@ public class A5Q1 {
         System.out.println("Enter a word to translate");
         String firstWord = input.nextLine();
         
+        //replace first a in the word to uba
         if (firstWord.contains("a" + "e" + "i" + "o" + "u")) {
            String translatedWordA = firstWord.replaceFirst("a", "uba");
             System.out.println(translatedWordA);
+            
+
         } else if (firstWord.contains("a")) {
             String translatedWordA = firstWord.replaceFirst("a", "uba");
             System.out.println(translatedWordA);
+            
+            //replace first e in the word to ube
         } else if (firstWord.contains("e")) {
             String translatedWordE = firstWord.replaceFirst("e", "ube");
             System.out.println(translatedWordE);
+            
+            //replace first i in the word to ubi
         } else if (firstWord.contains("i")) {
             String translatedWordI = firstWord.replaceFirst("i", "ubi");
             System.out.println(translatedWordI);
+            
+            //replace fist o to ubo           
         } else if (firstWord.contains("o")) {
             String translatedWordO = firstWord.replaceFirst("o", "ubo");
             System.out.println(translatedWordO);
+            
+            //replace first u to ubu
         }else if (firstWord.contains("u")) {
             String translatedWordU = firstWord.replaceFirst("u", "ubu");
-            System.out.println(translatedWordU);
+            System.out.println(translatedWordU);  
+            
         }
        
         
