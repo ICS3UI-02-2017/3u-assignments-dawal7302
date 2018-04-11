b0154043c5922d1da2fa1ed49af089263cb3bbe1
leendawamneh <leendawamneh@172.21.65.180>
2018-04-11 11:16:10 -0400

    working on A6Q4

--- src/Assignment6/A6Q4.java
+++ src/Assignment6/A6Q4.java
@@ -5,6 +5,8 @@
  */
 package Assignment6;
 
+import java.util.Scanner;
+
 /**
  *
  * @author leendawamneh
@@ -15,7 +17,26 @@ public class A6Q4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // create a scanner to store information
+        Scanner input = new Scanner(System.in);
+        
+        //ask user to enter 10 marks
+        System.out.println("Please enter 10 marks");
+        int markOne = input.nextInt();
+        int markTwo = input.nextInt();
+        int markThree = input.nextInt();
+        int markFour = input.nextInt();
+        int markFive = input.nextInt();
+        int markSix = input.nextInt();
+        int markSeven = input.nextInt();
+        int markEight = input.nextInt();
+        int markNine = input.nextInt();
+        int markTen = input.nextInt();
+        
+  
+        int highest = Integer.MIN_VALUE;
+        int lowest = Integer.MAX_VALUE;
+        
     }
     
 }
