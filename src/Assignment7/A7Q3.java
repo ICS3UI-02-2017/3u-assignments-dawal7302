41267f0dc2bc68756fe639378260b4c94a4f353e
leendawamneh <leendawamneh@172.21.65.180>
2018-04-30 11:15:37 -0400

    assignment 7 question 3

--- src/Assignment7/A7Q3.java
+++ src/Assignment7/A7Q3.java
@@ -5,17 +5,30 @@
  */
 package Assignment7;
 
+import java.util.Scanner;
+
 /**
  *
  * @author leendawamneh
  */
 public class A7Q3 {
+    public static void factorsOfNumber () {
+        Scanner input = new Scanner(System.in);
+        int numberFactored = input.nextInt();
+        int remainder = input.nextInt();
+        if (numberFactored % remainder == 0) {
+            int factors = numberFactored % remainder;
+            for(int numberFactored % remainder == 0) {
+        }
+        
+    } 
+    
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    
     }
     
 }
